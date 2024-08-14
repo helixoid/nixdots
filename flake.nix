a42fe34d5c1ebe660b2dd19a6a2f1d2a36df8d40
helix <ayushrajmth91@gmail.com>
2024-08-14 17:04:07 +0530

    HM added

--- flake.nix
+++ flake.nix
@@ -20,14 +20,13 @@
           };
         modules = [
           ./nixos/configuration.nix
-	homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "helix@nixos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
-        ];
+        
+	home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.helix = import ./home-manager/home.nix;
+	];
           };
         };
   };

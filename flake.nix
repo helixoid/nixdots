72e442c124fbbdc52f2e7ae2fae1eced53ad5bc9
helix <ayushrajmth91@gmail.com>
2024-08-14 17:07:59 +0530

    HM Removed

--- flake.nix
+++ flake.nix
@@ -5,30 +5,20 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { nixpkgs, home-manager, ... } @ inputs:
-{
+  {
     nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
 	 system = "x86_64-linux";
           specialArgs = {
             inherit inputs;
           };
-        modules = [
-          ./nixos/configuration.nix
-        
-	home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.helix = import ./home-manager/home.nix;
-	    }
-	];
-          };
-        };
+	  modules = [
+          ./nixos/configuration.nix	
+	  ];
+     	};
+    };
   };
 }

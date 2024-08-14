9de8242ee259e15e2024ac9f7cb525f795679403
helix <ayushrajmth91@gmail.com>
2024-08-14 17:39:34 +0530

    fu

--- flake.nix
+++ flake.nix
@@ -26,6 +26,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.helix = import ./home-manager/home.nix;
+	}
 	  ];
      	};
     };

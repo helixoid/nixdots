585f23fb40d9e9b9ceea00ebf97e6a352fc807ea
helix <ayushrajmth91@gmail.com>
2024-08-14 16:00:02 +0530

    fu

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -390,20 +390,20 @@
   # NixOS specialization named 'nvidia-sync'. Provides the ability
   # to switch the Nvidia Optimus Prime profile
   # to sync mode during the boot process, enhancing performance.
-  specialisation = {
-    nvidia-sync.configuration = {
-      system.nixos.tags = [ "nvidia-sync" ];
-      hardware.nvidia = {
-        powerManagement.finegrained = lib.mkForce false;
+  #specialisation = {
+   # nvidia-sync.configuration = {
+    #  system.nixos.tags = [ "nvidia-sync" ];
+     # hardware.nvidia = {
+      #  powerManagement.finegrained = lib.mkForce false;
 
-        prime.offload.enable = lib.mkForce false;
-        prime.offload.enableOffloadCmd = lib.mkForce false;
+       # prime.offload.enable = lib.mkForce false;
+        #prime.offload.enableOffloadCmd = lib.mkForce false;
 
-        prime.sync.enable = lib.mkForce true;
+        #prime.sync.enable = lib.mkForce true;
         # Dynamic Boost. It is a technology found in NVIDIA Max-Q design laptops with RTX GPUs.
         # It intelligently and automatically shifts power between
         # the CPU and GPU in real-time based on the workload of your game or application.
-        dynamicBoost.enable = lib.mkForce true;
+        #dynamicBoost.enable = lib.mkForce true;
       };
     };
   };
